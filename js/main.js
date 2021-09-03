0b442f1be1fa6d25bf1f73e5ce19ea768b9334fe
Na hyunwoo <dusdjeks@naver.com>
2021-09-03 10:14:14 +0900

    move prev

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const backToTop=document.getElementById('backtotop');
 
-function checkScroll(){
+const checkScroll = ()=>{
     let pageYOffset = window.pageYOffset;
 
     if(pageYOffset!==0){
@@ -10,7 +10,7 @@ function checkScroll(){
     }
 }
 
-function moveBackToTop(){
+const moveBackToTop =() =>{
     if(window.pageYOffset>0){
         window.scrollTo({top:0,behavior:"smooth"})
     }
@@ -19,4 +19,24 @@ function moveBackToTop(){
 window.addEventListener('scroll', checkScroll);
 backToTop.addEventListener('click',moveBackToTop);
 
+/*---------------------------------------------------------------*/
 
+const transformPrev = ()=>{
+    
+}
+
+const slidePrevList=document.getElementsByClassName('slide-prev');
+
+for(let i=0;i<slidePrevList.length;i++){
+    let classList=slidePrevList[i].parentElement.parentElement.nextElementSibling;
+    let liList=classList.getElementsByTagName('li');
+
+    if(classList.clientWidth<(liList.length*260)){
+        slidePrevList[i].classList.add('slide-prev-hover');
+        slidePrevList[i].addEventListener('click',transformPrev);
+    }else{
+        const arrowContainer = slidePrevList[i].parentNode;
+        arrowContainer.removeChild(slidePrevList[i].nextElementSibling);
+        arrowContainer.removeChild(slidePrevList[i]);
+    }
+}
